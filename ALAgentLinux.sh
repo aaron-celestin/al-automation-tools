d5ad348d8fc1e52931785f54716f15dd05f62447
aaron-celestin <118065165+aaron-celestin@users.noreply.github.com>
2022-11-17 15:46:31 -0600

    Upgraded Minor Fixes
    Check_enforce will now install python-utils if SELinux is enabled but semanage is unavailable
    Made REG_KEY implementation and its switches fault tolerant
    Set a final check to make sure agent service was running before reporting successful install
    Updated usage/help dialog

--- ALAgentLinux.sh
+++ ALAgentLinux.sh
@@ -14,7 +14,9 @@
 ##########################################################################################################
 
 function usage {
-    echo "Alert Logic Agent Installer $a_version
+    echo -e "
+--------------Alert Logic Agent Installer $a_version----------------
+
 Usage:" $0 "[-key <key>] | [-help]
     
     -key <key>      The key to provision the agent with.
@@ -36,7 +38,7 @@ Example: >" $0 "-key '1234567890abcdef1234567890aabcdef1234567890abcdef'
 Example: > source  "$0" -key '1234567890abcdef1234567890aabcdef1234567890abcdef'
 Example: > ./scriptname.sh -help
 
-It is strongly advised that this installer be run with sudo priviliges to ensure correct installation and configuration 
+It is strongly advised that this installer be run with sudo privileges to ensure correct installation and configuration 
 of the agent.
 
 For help, contact Alert Logic Technical Support.                
@@ -62,44 +64,44 @@ rpm32="https://scc.alertlogic.net/software/al-agent-LATEST-1.i386.rpm"
 rpm64="https://scc.alertlogic.net/software/al-agent-LATEST-1.x86_64.rpm"
 rpmarm="https://scc.alertlogic.net/software/al-agent-LATEST-1.aarch64.rpm"
 
-a_version="0.8.2"
+a_version="0.9.0"
 
 # Check whether RPM or DEB is installed
 function get_pkg_mgr () {
     pkg_mgr=""
-    if [ -f /usr/bin/dpkg ]; then       
+    if [[ -f /usr/bin/dpkg ]]; then       
         pkg_mgr="dpkg"
-    elif [ -f /usr/bin/rpm ]; then        
+    elif [[ -f /usr/bin/rpm ]]; then        
         pkg_mgr="rpm"
-    elif [ -n $(rpm --version) ]; then  # additional checks to verify which pkgmgr is installed in subshell     
+    elif [[ -n "$(rpm --version)" ]]; then  # additional checks to verify which pkgmgr is installed in subshell     
         pkg_mgr="rpm"
-    elif [ -n $(dpkg --version) ]; then  # the conditional checks are necessary because some distros have both rpm and dpkg installed      
+    elif [[ -n "$(dpkg --version)" ]]; then  # the conditional checks are necessary because some distros have both rpm and dpkg installed      
         pkg_mgr="dpkg"
     else
-        echo "Unable to determine package manager. Exiting."
+        echo -e "Unable to determine package manager. Exiting. "
         usage      
         exit 1
     fi
-    echo "Package manager is $pkg_mgr"
+    echo -e "Package manager is $pkg_mgr "
 }
 
 # Check CPU Architecture
 function get_arch () {
     thisarch=""
     arch=$(uname -i)
-    echo "Detected architecture is $arch"
-    if [[ $arch = x86_64 ]]; then
+    echo -e " Detected architecture is $arch"
+    if [[ $arch = "x86_64" ]]; then
         thisarch="x64"
-    elif [[ $arch = i*86 ]] || [[ $arch -eq i686 ]]; then
+    elif [[ $arch = "i*86" ]] || [[ $arch -eq i686 ]]; then
         thisarch="386"
-    elif  [[ $arch = arm* ]]; then
+    elif  [[ $arch = "arm*" ]]; then
         thisarch="ARM"
     else
-        echo "Unsupported architecture: $arch"
+        echo -e "Unsupported architecture: $arch"
         usage
         exit 1
     fi
-    echo "Architecture is $thisarch"
+    echo -e "Architecture is $thisarch"
 }
 
 # Setup and install the agent
@@ -147,37 +149,36 @@ function get_init_config () {
     elif [[ $(ps --noheaders -o comm 1) = "init" ]]; then
         init_type="init"
     else
-        echo "Unknown init configuration. Exiting."
+        echo -e "Unknown init configuration. Exiting."
         usage
         exit 1
     fi
-    echo "Detected init type is $init_type"
+    echo -e "Detected init type is $init_type "
 }
 
 # Configure the agent options
 function configure_agent () {
     get_init_config
- 
-    if [ ! -z $REG_KEY ]; then
+    if [[ -n "$REG_KEY" ]]; then
         sudo /etc/init.d/al-agent provision --key $REG_KEY
-        echo "Registration key set $REG_KEY"
     fi
-    if [ ! -z $proxy_ip ]; then
+    if [[ -n "$proxy_ip" ]]; then
         sudo /etc/init.d/al-agent configure --proxy $proxy_ip
-        echo "Proxy IP set $proxy_ip"
-    elif [ ! -z $proxy_host ]; then
+        echo -e "Proxy IP set $proxy_ip"
+    
+    elif [[ -n "$proxy_host" ]]; then
         sudo /etc/init.d/al-agent configure --proxy $proxy_host
-        echo "Proxy host set $proxy_host"
+        echo -e "Proxy host set $proxy_host"
     fi
 }
 
 # Configure SYSLOG Collection
 function make_syslog_config () {
-    if [[ -f $syslogng_conf_file ]]; then #check if given ngsyslog file exists
+    if [[ -f "$syslogng_conf_file" ]]; then #check if given ngsyslog file exists
         echo "destination d_alertlogic {tcp("localhost" port(1514));};" | sudo tee -a $syslogng_conf_file
         echo "log { source(s_sys); destination(d_alertlogic); };" | sudo tee -a $syslogng_conf_file
         sudo systemctl restart syslog-ng
-    elif [[ -f $syslog_conf_file ]]; then #check if given rsyslog file exists
+    elif [[ -f "$syslog_conf_file" ]]; then #check if given rsyslog file exists
         echo "*.* @@127.0.0.1:1514;RSYSLOG_FileFormat" | sudo tee -a $syslog_conf_file
         sudo systemctl restart rsyslog
     else
@@ -186,57 +187,85 @@ function make_syslog_config () {
 }
 
 # Check SELinux Status using semanage
-# If the semanage command is not present in your system, you can install the policycoreutils-python package to obtain the semanage command. 
+# If the semanage command is not present in your system, install the policycoreutils-python package to obtain the semanage command. 
 function check_enforce () {
-    se_status=$(getenforce)
-    echo "sestatus is $se_status"
-    if [[ $se_status ]]; then
-        if [ -n $(command -v semanage) ]; then 
-           if [[ $(getenforce) = "Permissive" ]]; then
-                sudo semanage port -a -t syslogd_port_t -p tcp 1514
-            elif [[ $(getenforce) = "Enforcing" ]]; then
-                sudo setenforce 0
-                sudo semanage port -a -t syslogd_port_t -p tcp 1514
-                sudo setenforce 1
+    if [[ $(getenforce 2>&1) =~ "command not found" ]]; then
+        echo -e "SELinux is not enabled. Continuing with installation..."
+    elif [[ $(getenforce) ]]; then
+        if [[ -n "$(command -v semanage)" ]]; then 
+            make_enforce
+        else
+        { 
+            echo -e "The semanage pkg is not available. Installing policycoreutils python utils package..."
+            if [[ -n $(sudo zypper --version 2>&1) ]]; then
+                sudo apt install policycoreutils-python-utils -y
+            elif [[ -n $(sudo apt --version 2>&1) ]]; then    
+                sudo zypper install --no-confirm policycoreutils-python-utils
+            elif [[ -n $(sudo yum --version 2>&1) ]]; then
+                sudo yum install policycoreutils-python-utils -y
             fi
-        else 
-            echo "SELinux is enabled but semanage is not installed. Please configure SELinux manually or install the policycoreutils-python package."
+            make_enforce    
+        }
         fi
-    else 
-        echo "SELinux is not enabled."
     fi 
 }
 
-# Install the agent with default settings
+
+function make_enforce () {    
+    if [[ $(getenforce) = "Permissive" ]]; then
+        sudo semanage port -a -t syslogd_port_t -p tcp 1514
+    elif [[ $(getenforce) = "Enforcing" ]]; then
+        sudo setenforce 0
+        sudo semanage port -a -t syslogd_port_t -p tcp 1514
+        sudo setenforce 1
+    fi
+}
+
+
+# Install the agent and configure it
 function run_install {
     install_agent
     check_enforce
     configure_agent
     make_syslog_config
-    echo "Install complete."   
-}
+    sudo /etc/init.d/al-agent start
+    if [[ -n "$(pgrep al-agent)" ]]; then
+        echo -e " Agent service started. Install complete. "
+    else 
+    { 
+        echo -e " Agent installed but service failed to start. Please check your system init and try again. "
+        exit 1
+    }
+    fi
+}       
 
 
 
-if [[ $1 = "-key" ]]; then
-    if [[ -z $2 ]]; then
-        echo "Registration key not provided. Exiting."
+if [[ $1 = "-key" ]] || [[ $1 = "--key" ]] || [[ $1 = "-k" ]]; then
+    if [[ -z "$2" ]]; then
+        echo -e "Key switch (-k|--key) set but no registration key was provided. Exiting."
         usage
         exit 1
     else
         REG_KEY=$2
-        echo "REG KEY is $REG_KEY"
+        echo -e "REG KEY is $REG_KEY"
         run_install
     fi
-    REG_KEY=$2
 elif [[ $1 = "-help" ]]; then
     usage
     exit 0
-elif [[ $# = 0 ]]; then
-    echo "REG KEY is in script: $REG_KEY"
+elif [[ $# = 0 ]] && [[ -n "$REG_KEY" ]]; then
+    echo -e  "REG KEY was retrieved from script section: $REG_KEY"
+    run_install
+elif [[ $# = 0 ]] && [[ -z "$REG_KEY" ]]; then
+    echo -e "No registration key provided. Check if agent script is in Cloud Environment (AWS, Azure)."
+    echo -e "Installation proceeding without a key. If agent is installed in a Datacenter environment, it will not provision without a key!"
     run_install
 else
-    echo "Invalid option(s): $@. Exiting."
+    echo -e  "Invalid option(s): $@. Exiting."
     usage
     exit 1
 fi
+
+
+#END SCRIPT
